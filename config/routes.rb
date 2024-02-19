b11a76755e5e4e4ed422bf2a177161c9600fffff
jinta-shimo02 <jinjinnbaaahi.0514@icloud.com>
2024-02-19 23:02:31 +0900

    ログアウトを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # OmniAuthによる認証が成功した場合、デフォルトで下記のURLがコールバックとして利用される。
   get "/auth/:provider/callback" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 end
